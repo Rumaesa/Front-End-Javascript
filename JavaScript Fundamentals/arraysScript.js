1df455afd7de8078a455d355e34e1ba762cf5e2a
Ansari Rumaesa <rumaesaansari@gmail.com>
2024-05-04 10:38:30 +0530

    Array Commit
    Committing Array in JS:
    1. created an Array.
    2. used multiple methods to modify array.
    3. completed a challenge.

---
+++ JavaScript Fundamentals/arraysScript.js
@@ -0,0 +1,121 @@
+"use strict";
+
+// Introduction to Arrays
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+// Array in JS is mutable.
+friends[2] = "Jay";
+console.log(friends);
+
+// friends = ['Bob', 'Alice']
+
+// An Array can store multiple types of data at the same time.
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+// To get the last value of an array
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
+
+// -----------------------------------------------
+
+// Basic Array operation (Methods - built in)
+/*
+Note: 
+Methods - built in
+Functions - user defined
+*/
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+//pushes item at the end of the array.
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+// Add elements at the start of an array
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
+// #CHALLENGE:
+
+/*
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+And now let's use arrays! So, create an array called bills containing the test data below.
+
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+*/
+
+// SOLUTION:
+function calcTip(bill) {
+  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return tip;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills);
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
